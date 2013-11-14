72a29c9be1cd8cf5a407c57b8b1b7bcbca44d37f
Christophe Promé <chprome@gmail.com>
2013-11-14 22:51:44 +0100

    Karma test runner pour les tests unitaires

---
+++ vanilla-examples/vanillajs/test/unit/karma.conf.js
@@ -0,0 +1,20 @@
+// an example karma.conf.js
+module.exports = function (config) {
+    config.set({
+        files: [
+            './init.js',
+            '../../js/store.js',
+            '../../js/model.js',
+            './specs/*.js'
+        ],
+        frameworks: ['mocha', 'chai', 'sinon'],
+        browsers: ['Chrome'],
+        autoWatch: true,
+        plugins: [
+            'karma-mocha',
+            'karma-chai',
+            'karma-sinon',
+            'karma-chrome-launcher'
+        ]
+    });
+};
